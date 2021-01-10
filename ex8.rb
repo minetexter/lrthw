a1c165db249518edfcb8252f0757600608670d3c
minetexter <han.jim.zhang@gmail.com>
2021-01-10 11:05:17 +0000

    ex8

---
+++ ex8.rb
@@ -0,0 +1,16 @@
+# Formats where to place the variables
+formatter = "%{first} %{second} %{third} %{fourth}"
+
+# Print out variables by key given format
+puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+puts formatter % {first: true, second: false, third: true, fourth: false}
+puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+
+# Can use multiple lines
+puts formatter % {
+  first: "I had this thing.",
+  second: "That you could type up right.",
+  third: "But it didn't sing.",
+  fourth: "So I said goodnight."
+}
